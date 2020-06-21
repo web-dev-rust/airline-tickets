708ce80e3951f2e790d8764c03213ae6ffb3b928
Julia Naomi <jnboeira@outlook.com>
2020-06-21 11:59:12 -0300

    Include CORS

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 extern crate juniper;
 extern crate serde_json;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, http::header};
+use actix_cors::Cors;
 
 mod boundaries;
 mod resolvers;
@@ -21,6 +22,17 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(resolvers.clone())
+            .wrap(
+                Cors::new()
+                    .allowed_origin("http://localhost:8080")
+                    .allowed_origin("http://127.0.0.1:8080")
+                    .allowed_origin("http://0.0.0.0:8080")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600)
+                    .finish(),
+            )
             .configure(routes)
             .default_service(web::to(|| async { "404" }))
     })
